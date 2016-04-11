92876d57f2da409ebdf762ef366a5ae4f45fcf99
motek96 <tborowicz@vp.pl>
2016-04-11 17:41:26 +0200

    Cpp

---
+++ fib.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+ 
+int fib(int n)
+{
+  if(n<3)
+    return 1;
+ 
+  return fib(n-2)+fib(n-1);
+}
+ 
+int main()
+{
+ 
+  int n;
+ 
+  cout<<"Podaj nr wyrazu ciągu: ";
+  cin>>n;
+ 
+  cout<<n<<" wyraz ciągu ma wartość "<<fib(n)<<endl;
+ 
+  system("pause");
+  return 0;
+}
