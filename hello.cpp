2e012fb858c21ba6380990da3489a97b44df946d
motek96 <tborowicz@vp.pl>
2016-04-11 17:43:22 +0200

    Cpp v2

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <stdafx.h>        //Powtarzam: tylko dla MVS 2010
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    cout<<"Hello World!";
+    cin.get();
+    return 0;
+}
